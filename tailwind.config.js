3980978793e9586747cd7dc2d3ff94053810e49a
Link Hsu <roccoisgood@gmail.com>
2024-02-02 04:24:57 +0800

    2024/2/2 - finish home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
+    },
   },
   variants: {},
   plugins: [],
